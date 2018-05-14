103376af8a75f2a029defa71779043584d4adecb
Chi_Liu <Amos_Lewis@foxmail.com>
2018-05-14 08:10:10 -0400

    add run file

---
+++ run.sh
@@ -0,0 +1,6 @@
+
+model_dir="seq2seq"
+if [ ! -d $model_dir ]; then
+	mkdir $model_dir
+fi
+python main.py --train_file data/train_mini.txt --dev_file data/dev_mini.txt --test_file data/test_mini.txt
